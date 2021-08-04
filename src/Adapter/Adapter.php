e4e7e90cc82fede204fdd8ad1f40e3a1775fef21
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-08-04 19:16:50 +0430

    Add Adapter

---
+++ src/Adapter/Adapter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mohammadv184\ArCaptcha\Adapter;
+
+interface Adapter
+{
+    /**
+     * Constructor
+     * @param string $site_key
+     * @param string $secret_key
+     * @param string $base_uri
+     */
+    public function __construct(string $site_key, string $secret_key, string $base_uri);
+
+    /**
+     * Submit Request
+     * @param string $challenge_id
+     * @return mixed
+     */
+    public function submit(string $challenge_id):array;
+}
